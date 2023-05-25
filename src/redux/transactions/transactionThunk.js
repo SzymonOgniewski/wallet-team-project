2bc408b585fd5e656435e66d49e864207cad80b7
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2023-05-25 22:57:19 +0200

    update axios url

--- src/redux/transactions/transactionThunk.js
+++ src/redux/transactions/transactionThunk.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-
+axios.defaults.baseURL = 'https://wallet.goit.ua/';
 export const fetchTransactions = createAsyncThunk(
   'transactions/fetchAll',
   async (_, thunkAPI) => {
