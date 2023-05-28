8398d89fe25bef7f89ce738c8f09c2b242e2daa6
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-05-28 14:25:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navigation from './Navigation/navigation';
 import CurrencyComponent from './Currency/Currency';
-import { RegistrationPage } from "../pages/RegistrationPage/RegistrationPage";
+import { RegistrationPage } from '../pages/RegistrationPage/RegistrationPage';
 
 const Home = () => <h1>Home Page</h1>;
 const Diagram = () => <h1>Diagram Page</h1>;
@@ -11,14 +11,14 @@ const Currency = () => <CurrencyComponent />;
 export const App = () => {
   return (
     <>
-        {/* <Navigation /> */}
-        <Routes>
-          <Route path="*" exact element={<Home />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route path="/diagram" element={<Diagram />} />
-          <Route path="/currency" element={<Currency />} />
-        </Routes>
-        {/* <CurrencyComponent /> */}
+      {/* <Navigation /> */}
+      <Routes>
+        <Route path="*" exact element={<Home />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/diagram" element={<Diagram />} />
+        <Route path="/currency" element={<Currency />} />
+      </Routes>
+      {/* <CurrencyComponent /> */}
     </>
   );
-}
+};
