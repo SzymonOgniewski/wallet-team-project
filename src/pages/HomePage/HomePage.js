03d260ed6126e84dc31ca76eb95e8153d91894ed
Mateusz Wójcikowski <yankes90@gmail.com>
2023-06-01 19:37:38 +0200

    Movments
    Functions generations for MainPage relocated to Pages

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,68 @@
+import Media from 'react-media';
+import Header from 'components/Header/Header';
+import Navigation from 'components/Navigation/navigation';
+import TotalBalanceComponent from 'components/Balance/Balance';
+import HomeTab from 'components/HomeTab/HomeTab';
+import CurrencyComponent from 'components/Currency/Currency';
+
+import styles from './HomePage.module.css';
+
+export const Home = () => (
+  <>
+    <Media
+      query="(max-width: 767px)"
+      render={() => (
+        <>
+          <Header />
+          <Navigation />
+          <TotalBalanceComponent />
+          <HomeTab />
+        </>
+      )}
+    />
+
+    <>
+      <Media
+        query="(min-width: 768px) and (max-width: 1279px)"
+        render={() => (
+          <>
+            <Header />
+            <div className={styles.blocks}>
+              <div>
+                <Navigation />
+                <TotalBalanceComponent />
+              </div>
+              <div>
+                <CurrencyComponent />
+              </div>
+            </div>
+            <div className={styles.homeTab}>
+              <HomeTab />
+            </div>
+          </>
+        )}
+      />
+    </>
+    <>
+      <Media
+        query="(min-width: 1280px)"
+        render={() => (
+          <>
+            <Header />
+            <div className={styles.blocks}>
+              <div className={styles.leftBlock}>
+                <Navigation />
+                <TotalBalanceComponent />
+                <CurrencyComponent />
+              </div>
+              <div>
+                {' '}
+                <HomeTab />
+              </div>
+            </div>
+          </>
+        )}
+      />
+    </>
+  </>
+);
