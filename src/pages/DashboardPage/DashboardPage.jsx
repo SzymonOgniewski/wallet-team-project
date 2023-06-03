812831b23aed4b4a1ebddfe0d2a5d2cbff195351
ElzbietaOskiera <elaoskiera96@gmail.com>
2023-06-03 18:24:36 +0200

    Update DashboardPage.jsx

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import HomeTab from 'components/HomeTab/HomeTab'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import HomeTab from 'components/HomeTab/HomeTab';
 import { ButtonAddTransactions } from 'components/ButtonAddTransactions/ButtonAddTransactions';
 import DashboardLayout from 'components/DashboardLayout/DashboardLayout';
+import { fetchBalance } from 'redux/finance/financeThunks';
+import { fetchTransactions } from 'redux/transactions/transactionThunk';
 
 export function DashboardPage() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBalance());
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
   return (
     <DashboardLayout>
       <HomeTab />
