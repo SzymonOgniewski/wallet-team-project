e4b5665c9e64f743de08e777f5dd49ef2118a3c9
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2023-08-16 21:04:02 +0200

    updated Registration confirm

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -56,7 +56,6 @@ export const Home = () => (
                 <CurrencyComponent />
               </div>
               <div>
-                {' '}
                 <HomeTab />
               </div>
             </div>
